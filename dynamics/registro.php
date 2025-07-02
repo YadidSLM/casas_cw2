6ac1fbfbd8b7ac8a6881ddafb98db0fac630ef3a
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2025-07-02 14:43:18 -0600

    No recuerdo qué cambios hicimos, pero hace el registro a la BD

--- dynamics/registro.php
+++ dynamics/registro.php
@@ -7,8 +7,24 @@
 </head>
 <body>
 <?php
-    
+    include("./config.php");
+    $conexion = connect();
 
+    $nombre = (isset($_POST["nombre"]) && $_POST["nombre"] != "") ? $_POST["nombre"] : "No se ingresa valor";
+    $apodo = (isset($_POST["apodo"]) && $_POST["apodo"] != "") ? $_POST["apodo"] : "No se ingresa valor";
+    $casa = (isset($_POST["casa"]) && $_POST["casa"] != "") ? $_POST["casa"] : "No se ingresa valor";
+
+    $casas = [
+        "halcones" => 1,
+        "teporingos" => 2,
+        "vaquitas" => 3,
+        "ajolotes" => 4,
+    ];
+
+    $peticion = "INSERT INTO USUARIO (NOMBRE,APODO,ID_CASA) VALUES ('$nombre','$apodo',".$casas[$casa].")";
+    //Petición
+    $query = mysqli_query($conexion, $peticion);
+    header("location:../inicio.php");
 ?>
 </body>
 </html>
